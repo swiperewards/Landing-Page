7cf8bb2ca43bb8d950f1f016d7a6cd7271987911
Janek <teojanekathryn@gmail.com>
2018-07-30 13:43:30 +0800

    :tada: client landing page

--- js/new-age.js
+++ js/new-age.js
@@ -1,6 +1,32 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  /*----------------------------------------------------*/
+	/* Adjust Primary Navigation Background Opacity
+  ------------------------------------------------------*/
+  $(window).on("scroll", function () {
+    console.log("shit");
+
+      var h = $('#mainNav').height();
+      var y = $(window).scrollTop();
+      var header = $('#mainNav');
+
+      if ((y > h + 30 ) && ($(window).outerWidth() > 768 ) ) {
+        header.addClass('opaque');	 
+        console.log("shit2");
+     
+      }
+      else {
+        console.log("shit3");
+        if (y < h + 30) {
+          header.removeClass('opaque');
+        }
+        else {
+          header.addClass('opaque');
+        }
+      } 
+  });
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -27,16 +53,18 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
+  // var navbarCollapse = function() {
+  //   if ($("#mainNav").offset().top > 100) {
+  //     $("#mainNav").addClass("navbar-shrink");
+  //   } else {
+  //     $("#mainNav").removeClass("navbar-shrink");
+  //   }
+  // };
   // Collapse now if page is not at top
-  navbarCollapse();
+  // navbarCollapse();
   // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+  // $(window).scroll(navbarCollapse);
+
+  
 
 })(jQuery); // End of use strict
